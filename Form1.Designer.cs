ffdf7030ac6148fa3d27551ecaa99e3eb7eadd04
Ablac <ablac.nezgolur@gmail.com>
2021-11-05 10:39:42 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Swoger_CourseProject_Part1
+{
+    partial class VideoManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.artistText = new System.Windows.Forms.TextBox();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.yearText = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.genreText = new System.Windows.Forms.TextBox();
+            this.genreLabel = new System.Windows.Forms.Label();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.detailsLabel = new System.Windows.Forms.Label();
+            this.allSongsButton = new System.Windows.Forms.Button();
+            this.songListLabel = new System.Windows.Forms.Label();
+            this.songList = new System.Windows.Forms.ListBox();
+            this.titelName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.addButton.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold);
+            this.addButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.addButton.Location = new System.Drawing.Point(122, 451);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(195, 78);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add Song";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.Location = new System.Drawing.Point(117, 208);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(66, 29);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Title";
+            this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+            // 
+            // titleText
+            // 
+            this.titleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleText.Location = new System.Drawing.Point(214, 201);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(335, 35);
+            this.titleText.TabIndex = 0;
+            // 
+            // artistText
+            // 
+            this.artistText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artistText.Location = new System.Drawing.Point(214, 242);
+            this.artistText.Name = "artistText";
+            this.artistText.Size = new System.Drawing.Size(335, 35);
+            this.artistText.TabIndex = 1;
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.artistLabel.Location = new System.Drawing.Point(117, 249);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(72, 29);
+            this.artistLabel.TabIndex = 3;
+            this.artistLabel.Text = "Artist";
+            // 
+            // yearText
+            // 
+            this.yearText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearText.Location = new System.Drawing.Point(214, 324);
+            this.yearText.Name = "yearText";
+            this.yearText.Size = new System.Drawing.Size(335, 35);
+            this.yearText.TabIndex = 3;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearLabel.Location = new System.Drawing.Point(117, 331);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(68, 29);
+            this.yearLabel.TabIndex = 7;
+            this.yearLabel.Text = "Year";
+            // 
+            // genreText
+            // 
+            this.genreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genreText.Location = new System.Drawing.Point(214, 283);
+            this.genreText.Name = "genreText";
+            this.genreText.Size = new System.Drawing.Size(335, 35);
+            this.genreText.TabIndex = 2;
+            // 
+            // genreLabel
+            // 
+            this.genreLabel.AutoSize = true;
+            this.genreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.genreLabel.Location = new System.Drawing.Point(117, 290);
+            this.genreLabel.Name = "genreLabel";
+            this.genreLabel.Size = new System.Drawing.Size(85, 29);
+            this.genreLabel.TabIndex = 5;
+            this.genreLabel.Text = "Genre";
+            // 
+            // urlText
+            // 
+            this.urlText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlText.Location = new System.Drawing.Point(214, 365);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(335, 35);
+            this.urlText.TabIndex = 4;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlLabel.Location = new System.Drawing.Point(117, 372);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(63, 29);
+            this.urlLabel.TabIndex = 9;
+            this.urlLabel.Text = "URL";
+            // 
+            // outputText
+            // 
+            this.outputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputText.Location = new System.Drawing.Point(636, 451);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputText.Size = new System.Drawing.Size(583, 352);
+            this.outputText.TabIndex = 999;
+            this.outputText.TabStop = false;
+            // 
+            // detailsLabel
+            // 
+            this.detailsLabel.AutoSize = true;
+            this.detailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.detailsLabel.Location = new System.Drawing.Point(631, 419);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(94, 29);
+            this.detailsLabel.TabIndex = 15;
+            this.detailsLabel.Text = "Details";
+            // 
+            // allSongsButton
+            // 
+            this.allSongsButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.allSongsButton.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.allSongsButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.allSongsButton.Location = new System.Drawing.Point(354, 451);
+            this.allSongsButton.Name = "allSongsButton";
+            this.allSongsButton.Size = new System.Drawing.Size(195, 78);
+            this.allSongsButton.TabIndex = 6;
+            this.allSongsButton.Text = "Show All Songs";
+            this.allSongsButton.UseVisualStyleBackColor = false;
+            this.allSongsButton.Click += new System.EventHandler(this.allSongsButton_Click);
+            // 
+            // songListLabel
+            // 
+            this.songListLabel.AutoSize = true;
+            this.songListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.songListLabel.Location = new System.Drawing.Point(631, 50);
+            this.songListLabel.Name = "songListLabel";
+            this.songListLabel.Size = new System.Drawing.Size(122, 29);
+            this.songListLabel.TabIndex = 17;
+            this.songListLabel.Text = "Song List";
+            // 
+            // songList
+            // 
+            this.songList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.songList.FormattingEnabled = true;
+            this.songList.HorizontalScrollbar = true;
+            this.songList.ItemHeight = 29;
+            this.songList.Location = new System.Drawing.Point(636, 82);
+            this.songList.Name = "songList";
+            this.songList.Size = new System.Drawing.Size(583, 323);
+            this.songList.TabIndex = 998;
+            this.songList.TabStop = false;
+            // 
+            // titelName
+            // 
+            this.titelName.AutoSize = true;
+            this.titelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titelName.Location = new System.Drawing.Point(122, 50);
+            this.titelName.Name = "titelName";
+            this.titelName.Size = new System.Drawing.Size(493, 46);
+            this.titelName.TabIndex = 997;
+            this.titelName.Text = "CEIS 209 Video Manager";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 37);
+            this.label1.TabIndex = 996;
+            this.label1.Text = "by Keith Swoger";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(207, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 37);
+            this.label2.TabIndex = 995;
+            this.label2.Text = "November 2021";
+            // 
+            // VideoManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 849);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titelName);
+            this.Controls.Add(this.songList);
+            this.Controls.Add(this.songListLabel);
+            this.Controls.Add(this.allSongsButton);
+            this.Controls.Add(this.detailsLabel);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.yearText);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.genreText);
+            this.Controls.Add(this.genreLabel);
+            this.Controls.Add(this.artistText);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.titleText);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.addButton);
+            this.Name = "VideoManager";
+            this.Text = "Video Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.TextBox artistText;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.TextBox yearText;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox genreText;
+        private System.Windows.Forms.Label genreLabel;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Label detailsLabel;
+        private System.Windows.Forms.Button allSongsButton;
+        private System.Windows.Forms.Label songListLabel;
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.Label titelName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Swoger_CourseProject_Part1
+{
+    public partial class VideoManager : Form
+    {
+        public VideoManager()
+        {
+            InitializeComponent();
+        }
+
+        //Add Song Button
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //Error Checking/Not Blank
+            if (string.IsNullOrEmpty(titleText.Text)) //Check Title
+                MB("The Title can't be blank!"); //Message Box Pass Through Title
+            else if (string.IsNullOrEmpty(artistText.Text)) //Check Artist
+                MB("The Artist can't be blank!"); //Message Box Pass Through Artist
+            else if (string.IsNullOrEmpty(genreText.Text)) //Check Genre
+                MB("The Genre can't be blank!"); //Message Box Pass Through Genre
+            else if (string.IsNullOrEmpty(yearText.Text)) //Check Year
+                MB("The Year can't be blank!"); //Message Box Pass Through Year
+            else if (string.IsNullOrEmpty(urlText.Text)) //Check URL
+                MB("The URL can't be blank!"); //Message Box Pass Through URL
+            else
+            {
+                //Build the output text
+                songList.Items.Add(titleText.Text);
+                clear(); //Clear Textboxes and Set Focus to Title
+            }
+        }
+
+        //View all songs button
+        private void allSongsButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(string.Empty);
+
+            //Shortcut for New Line
+            string nl = "\r\n";
+
+            //Build the Output Text
+            foreach (var item in songList.Items)
+            {
+                sb.Append(item.ToString());
+                sb.Append(nl);
+            }
+            //Put the output text into the output box
+            outputText.Text = sb.ToString();
+        }
+
+        //Clear's all text from boxes, and moves focus to Title Box.
+        private void clear()
+        {
+            //Clears Text
+            titleText.Text = String.Empty;
+            artistText.Text = String.Empty;
+            genreText.Text = String.Empty;
+            yearText.Text = String.Empty;
+            urlText.Text = String.Empty;
+
+            //Sets Focus
+            titleText.Focus();
+        }
+
+        private void MB(string Text)
+        {
+            MessageBox.Show(Text, "ERROR!",
+                    System.Windows.Forms.MessageBoxButtons.OKCancel,
+                    System.Windows.Forms.MessageBoxIcon.Error);
+        }
+        private void titleLabel_Click(object sender, EventArgs e)
+        {
+            MB("This is an EasterEgg LOL!");
+        }
+    }
+}
+
+/* Build Output Text Saved Code.
+            StringBuilder sb = new StringBuilder(string.Empty);
+
+            //Shortcut for New Line
+            string nl = "\r\n"; 
+
+//Build the output Text
+                sb.Append(titleText.Text);
+                sb.Append(nl);
+                sb.Append(artistText.Text);
+                sb.Append(nl);
+                sb.Append(genreText.Text);
+                sb.Append(nl);
+                sb.Append(yearText.Text);
+                sb.Append(nl);
+                sb.Append(urlText.Text);
+                sb.Append(nl);
+
+                //Output built text
+                outputText.Text = sb.ToString(); 
+*/
