9b39309b146d87c2b0f75634ca976d89f8e627ad
Ablac <ablac.nezgolur@gmail.com>
2021-11-08 10:36:16 -0800

    Moved Functions to Functions.CS for easier management of code. Added additional Comments for easier code management.

---
+++ Form.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* CEIS 209
+ * Devry University
+ * Keith S
+ * 11/5/2021
+*/
+
+namespace Swoger_CourseProject_Part1
+{
+    public partial class VideoManager : Form
+    {
+        //***********************************************************************************************************
+        //**************************************************BUTTONS**************************************************
+        //***********************************************************************************************************
+        public VideoManager()
+        {
+            InitializeComponent();
+        }
+        //*************************
+        //*****Add Song Button*****
+        //*************************
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(outputText.Text);
+            string nl = "\r\n";
+
+            if(ValidInput())
+            {
+                //Build the output Text
+                sb.Append(titleText.Text);
+                sb.Append(nl);
+                sb.Append(artistText.Text);
+                sb.Append(nl);
+                sb.Append(genreText.Text);
+                sb.Append(nl);
+                sb.Append(yearText.Text);
+                sb.Append(nl);
+                sb.Append(urlText.Text);
+                sb.Append(nl);
+
+                //Output built text
+                outputText.Text = sb.ToString();
+                songList.Items.Add(titleText.Text);
+                clear();
+            }
+        }
+        //*******************************
+        //*****View all songs button*****
+        //*******************************
+        private void allSongsButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(string.Empty);
+
+            //Shortcut for New Line
+            string nl = "\r\n";
+
+            //Build the Output Text
+            foreach (var item in songList.Items)
+            {
+                sb.Append(item.ToString());
+                sb.Append(nl);
+            }
+            //Put the output text into the output box
+            outputText.Text = sb.ToString();
+        }
+        //**************************
+        //*****Find Song Button*****
+        //**************************
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if (SongInList(titleText.Text))
+            {
+                MB($"Song {titleText.Text} Found", "Found Song!", 4);
+            }
+            else
+                MB($"Song {titleText.Text} not found", "Error!", 1);
+        }
+        //**********************
+        //*****Clear Button*****
+        //**********************
+        private void clearSong_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+        //***************************
+        //*****Easter Egg Button*****
+        //***************************
+        private void titleLabel_Click(object sender, EventArgs e)
+        {
+            MB("This is an EasterEgg LOL!", "Easter Egg :D", 3);
+        }
+    }
+}
+//******************************************************************
+//**************FUNCTIONS CAN BE FOUND IN FUNCTIONS.CS**************
+//******************************************************************
---
+++ Functions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swoger_CourseProject_Part1
+{
+    partial class VideoManager
+    {
+        //***********************************************************************************************************
+        //*************************************************FUNCTIONS*************************************************
+        //***********************************************************************************************************
+        //****************************
+        //*****Song List Function*****
+        //****************************
+        private bool SongInList(string SongTitle)
+        {
+            bool inList = false;
+            foreach (var item in songList.Items)
+            {
+                string currentSong = item as string;
+                if (SongTitle == currentSong)
+                {
+                    inList = true;
+                }
+            }
+            return inList;
+        }
+        //*******************************
+        //*****Verify Input Function*****
+        //*******************************
+        private bool ValidInput()
+        {
+            bool isValid = true;
+            if (string.IsNullOrEmpty(titleText.Text)) //Check Title
+            {
+                MB("The Title can't be blank!", "Error!", 1); //Message Box Pass Through Title
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(artistText.Text)) //Check Artist
+            {
+                MB("The Artist can't be blank!", "Error!", 1); //Message Box Pass Through Artist
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(genreText.Text)) //Check Genre
+            {
+                MB("The Genre can't be blank!", "Error!", 1); //Message Box Pass Through Genre
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(yearText.Text)) //Check Year
+            {
+                MB("The Year can't be blank!", "Error!", 1); //Message Box Pass Through Year
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(urlText.Text)) //Check URL
+            {
+                MB("The URL can't be blank!", "Error!", 1); //Message Box Pass Through URl
+                isValid = false;
+            }
+            return isValid;
+        }
+        //************************
+        //*****Clear Function*****
+        //************************
+        private void clear() //Method to clear all text box's
+        {
+            //Clears Text
+            titleText.Text = String.Empty;
+            artistText.Text = String.Empty;
+            genreText.Text = String.Empty;
+            yearText.Text = String.Empty;
+            urlText.Text = String.Empty;
+
+            //Sets Focus
+            titleText.Focus();
+        }
+        //******************************
+        //*****Message Box Function*****
+        //******************************
+        private void MB(string Text, String Title, int Icon)
+        {
+            if (Icon == 1) //ERROR
+                MessageBox.Show(Text, Title, System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Error);
+            else if (Icon == 2) //QUESTION
+                MessageBox.Show(Text, Title, System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Question);
+            else if (Icon == 3) //Exclamation
+                MessageBox.Show(Text, Title, System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Exclamation);
+            else if (Icon == 4) //Information
+                MessageBox.Show(Text, Title, System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Information);
+        }
+    }
+    }
